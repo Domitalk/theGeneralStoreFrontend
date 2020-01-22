1083d8036a50fd35318047ddc13cb8aeeb4edd1f
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-22 11:29:04 -0500

    morrn

--- my-project-client/src/components/Profile.js
+++ my-project-client/src/components/Profile.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default class Profile extends React.Component {
     state = {
-        carts: []
+        carts: [], 
+        loadeverything: false
     }
     makeCreatedAtPretty = (created_at) => {
+        console.log(created_at)
         if (this.props.user.created_at) {
             let copyString = created_at
             return copyString.substring(0, 10)
@@ -14,46 +16,103 @@ export default class Profile extends React.Component {
 
         /// for some reason this was crashing my authlogin
 
+    // componentDidMount() {
+    //     if (this.state.carts === 0) {
+    //         fetch(`http://localhost:4000/carts/${localStorage.user_id}`)
+    //         .then(r => r.json())
+    //         .then((response) => {
+    //             this.setState({
+    //                 carts: response
+    //             }, () => {console.log(this.state)})
+    //         })
+    //     }
+    // }
 
-    checkme = () => {
-        if (this.props.user > 0) {
-            fetch(`http://localhost:4000/users/${localStorage.user_id}`)
+    // checkme = () => {
+    //     if (this.state.carts === 0) {
+    //         fetch(`http://localhost:4000/carts/${localStorage.user_id}`)
+    //         .then(r => r.json())
+    //         .then((response) => {
+    //             this.setState({
+    //                 carts: response
+    //             }, () => {console.log(this.state)})
+    //         })
+    //     }
+    // }
+
+    // showPreviousCarts = () => {
+    //     // this.checkme()
+    //     return (
+    //         <ul>
+    //             {this.mapAllCarts()}
+    //         </ul>
+    //     )
+    // }
+
+    // mapAllCarts = () => {
+    //     if (this.state.carts.length > 0) {
+    //         return this.state.carts.map((cart) => {
+    //             return (
+    //                 <li>
+    //                     Shopping Cart Created: {this.makeCreatedAtPretty(cart.created_at)}, Updated: {this.makeCreatedAtPretty(cart.updated_at)}
+    //                 </li>
+    //             )
+    //         })
+    //     }
+    // }
+
+
+    //// come back and finish showing all the profile elements
+
+    componentDidMount() {
+        if (!!localStorage.user_id) {
+            fetch(`http://localhost:4000/carts/${localStorage.user_id}`, {
+                method: "GET", 
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json', 
+                    "Authorization": localStorage.token
+                }
+            })
             .then(r => r.json())
             .then((response) => {
+                
                 this.setState({
-                    carts: response
+                    carts: response, 
+                    loadeverything: true
                 })
             })
         }
     }
 
-    showPreviousCarts = () => {
-        return (
-            <ul>
-                {this.mapAllCarts()}
-            </ul>
+    mapAllCarts = () => {
+        return (this.state.carts.map((cart) => {
+            console.log(cart)
+            return <li>Shopped on: {this.makeCreatedAtPretty(cart.created_at)}</li>
+            })
         )
     }
 
-    mapAllCarts = () => {
-        if (this.state.carts.length > 0) {
-            return this.state.carts.map((cart) => {
-                return (
-                    <li>
-                        Shopping Cart Created: {this.makeCreatedAtPretty(cart.created_at)}, Updated: {this.makeCreatedAtPretty(cart.updated_at)}
-                    </li>
-                )
-            })
-        }
+    loadstuff = () => {
+        return (
+            <div>
+                <h1>{localStorage.name}</h1>
+                <img src={localStorage.picture} />
+
+                <ul>
+                    {this.mapAllCarts()}
+                </ul>
+            </div>
+        )
     }
 
     render() {
         // console.log(this.props)
+        // console.log(this.state)
 
         return (
             <div>
-                {this.props.loggedIn ? <h1>{localStorage.name}</h1> : null}
-                {this.props.loggedIn ? this.showPreviousCarts() : null }
+                {this.state.loadeverything ? this.loadstuff() : null}
             </div>
         )
     }
